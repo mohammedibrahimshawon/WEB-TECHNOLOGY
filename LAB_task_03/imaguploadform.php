ecb0a49009a7230eb6ca64644cb3ff49a97ed7bc
IBRAHIM SHAWON <mshawonc3@gmail.com>
2021-10-20 00:22:06 +0600

    LABTASK 03
    LABTASK 03

---
+++ LAB_task_03/imaguploadform.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PROFILE PICTURE</title>
+</head>
+<body>
+<h3 align="">PROFILE PICTURE</h3>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+ <br> <br> <br>  <br>  <br>  <br> 
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <br> 
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
